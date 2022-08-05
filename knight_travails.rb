af7edcd65509b58ffabc442505b1c3969b8729a2
Humberto <hdkaratechop@gmail.com>
2022-08-04 22:55:55 -0700

    Add Knight class and knigh_move method.

---
+++ knight_travails.rb
@@ -0,0 +1,13 @@
+class Knight
+    attr_accessor :start_point , :stop_point 
+    def initialize(start_point, stop_point)
+        @start_point = start_point
+        @stop_point = stop_point
+
+    # a knight can move up one place and two to the side 
+    def knigh_move
+         if x >= 0 & x <= 8 & y >= 0 & y <= 8 
+
+    end
+
+end
