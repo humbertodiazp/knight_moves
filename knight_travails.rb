4e0b3126890bc1901d0bed2996421cc68027c48d
Humberto <hdkaratechop@gmail.com>
2022-08-05 23:28:02 -0700

    Define knight_move. Add logic.

--- knight_travails.rb
+++ knight_travails.rb
@@ -1,13 +1,19 @@
 class Knight
-    attr_accessor :start_point , :stop_point 
-    def initialize(start_point, stop_point)
+    attr_accessor :start_point , :end_point 
+    def initialize(start_point, end_point)
         @start_point = start_point
-        @stop_point = stop_point
+        @end_point = stop_point
 
     # a knight can move up one place and two to the side 
-    def knigh_move
-         if x >= 0 & x <= 8 & y >= 0 & y <= 8 
+    def knigh_move(start_point)
+        x = start_point[0] += 1
+        y= end_point[1] += 1 
+    
+         if start_point[0] >= 0 & start_point[0] 
+        
+        if end_point    <= 8 & y >= 0 & y <= 8 
+
 
     end
-
 end
+
