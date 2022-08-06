f309e76aa85b348f82c678407dd00cf1e88c2731
Humberto <hdkaratechop@gmail.com>
2022-08-06 02:26:39 -0700

    Add recursive call to knight_move until reaches end_point

--- knight_travails.rb
+++ knight_travails.rb
@@ -2,16 +2,28 @@ class Knight
     attr_accessor :start_point , :end_point 
     def initialize(start_point, end_point)
         @start_point = start_point
-        @end_point = stop_point
+        @end_point = end_point
+    end
+
 
     # a knight can move up one place and two to the side 
-    def knigh_move(start_point)
+    def knigh_move(start_point, end_point)
         x = start_point[0] 
-        y= end_point[1] 
+        y = start_point[1] 
+        a = end_point[0] 
+        b = end_point[1] 
         # logic herer needs to be improved
-         if x >= 0 & x[0] <= 8 & if y >= 0 & y <= 8 
-            start_point[0] += 1
-            end_point[1] += 1 
+         if x > 8 || y > 8 || a > 8 || b > 8 |
+
+            return 'invalid move, outside of play area'
+         else 
+            x += 1 & y =+ 2 
+            puts start_point
+        end
+            return knigh_move(start_point, end_point)
+
+        end
     end
+
 end
 
