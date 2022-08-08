0c0a4232e5fb9b3746c922895256b3f9a4f05622
Humberto <hdkaratechop@gmail.com>
2022-08-07 18:13:48 -0700

    Set conditional logic to determine knight movements based on start_point

--- knight_travails.rb
+++ knight_travails.rb
@@ -7,23 +7,46 @@ class Knight
 
 
     # a knight can move up one place and two to the side 
-    def knigh_move(start_point, end_point)
-        x = start_point[0] 
-        y = start_point[1] 
-        a = end_point[0] 
-        b = end_point[1] 
+    def knight_moves(start_point, end_point)
+        if  a > 8 || b > 8 
+            return 'invalid move, outside of board'
+        end 
+        x = start_point[0].to_s 
+        y = start_point[1].to_s
+        a = end_point[0].to_s 
+        b = end_point[1].to_s
         # logic herer needs to be improved
-         if x > 8 || y > 8 || a > 8 || b > 8 |
-
-            return 'invalid move, outside of play area'
-         else 
-            x += 1 & y =+ 2 
-            puts start_point
+         while x & y < a & b 
+            x += 1 & y += 2 
+            puts "[#{x}, #{y}]"
         end
-            return knigh_move(start_point, end_point)
-
-        end
+            return knight_moves(x, y)
     end
-
 end
 
+def knight_moves(start_point, end_point)
+
+    # the knight has to move up or down depending on whether the end point is higher or lower than the start.
+    # it needs to do this based on going up 1 and 2 to the side, or vice versa. 
+    # need a conditional statement that determines whether the knight moves up or down, i.e. adds to x or subtracts
+    # and the same goes for y. 
+    a = end_point[0]
+    puts a
+    b = end_point[1]
+    puts b
+    if  a > 8 || b > 8 
+        return 'invalid move, outside of board'
+    end 
+    x = start_point[0]
+    p x
+    y = start_point[1]
+    p y 
+    
+    # logic herer needs to be improved
+     while x <= a && b <= y
+        x += 1 & y += 2 
+        puts "[#{x}, #{y}]"
+    end
+end
+
+puts knight_moves([0,0], [8,8])
